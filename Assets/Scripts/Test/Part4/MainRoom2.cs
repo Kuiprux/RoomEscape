caeb0286a3091408020da9e5f66a23459668b949
Kuiprux <kuiprux@gmail.com>
2023-11-10 00:01:16 +0900

    Merged 2-3

--- Assets/Scripts/Test/Part4/MainRoom2.cs
+++ Assets/Scripts/Test/Part4/MainRoom2.cs
@@ -8,11 +8,15 @@ public class MainRoom2 : Room
     new void Start()
     {
         base.Start();
+        transform.Find("Memo").gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Room2_Picture.instance.find >= 3)
+        {
+            transform.Find("Memo").gameObject.SetActive(true);
+        }
     }
 }
